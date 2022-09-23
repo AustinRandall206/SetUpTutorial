f77c95d4c0ca8593aeafcb6550deb8fb5f7a8bd9
Austin <austinrandall206@gmail.com>
2022-09-23 14:15:46 -0600

    figuring it out

--- JavaClass/src/main/SetUp.java
+++ JavaClass/src/main/SetUp.java
@@ -4,4 +4,7 @@ public class SetUp {
     public static void main(String[] args) {
 
     }
+    public int twoPlusTwo(int a, int b){
+       return 4;
+    }
 }
--- JavaClass/test/SetUpTest.java
+++ JavaClass/test/SetUpTest.java
@@ -1,4 +1,21 @@
 package test;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+import src.main.SetUp;
+
 public class SetUpTest {
+    private SetUp setUp;
+
+    public SetUpTest(){
+        setUp= new SetUp();
+    }
+    @Test
+    public void twoPlusTwo(){
+        int x = 2;
+        int y = 2;
+        setUp.twoPlusTwo(x,y);
+    }
+
 }
